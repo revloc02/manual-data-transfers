f3cdd04041c7b6ec3ac70837da8d74b01e6479ca
Forest Allan Colver Sr <fcolver@churchofjesuschrist.org>
2022-01-19 06:36:18 -0700

    initial commit

---
+++ src/main/java/forest/colver/datatransfer/Main.java
@@ -0,0 +1,12 @@
+package forest.colver.datatransfer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+  private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+  public static void main(String[] args) {
+    LOG.info("log");
+  }
+}
