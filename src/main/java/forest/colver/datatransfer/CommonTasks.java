cf0c7dc446c06158e921ad430f8efc9610a8a593
revloc02 <4457735+revloc02@users.noreply.github.com>
2022-01-22 13:22:25 -0700

    adding skeleton for common tasks

---
+++ src/main/java/forest/colver/datatransfer/CommonTasks.java
@@ -0,0 +1,7 @@
+package forest.colver.datatransfer;
+
+public class CommonTasks {
+  // todo: a way to clean up sftp-error sqs removing only certain messages e.g. all health check errors and not interchange errors
+
+  // todo: common watchman tasks
+}
