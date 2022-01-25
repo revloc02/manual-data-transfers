3ea4afe79cea0b3eba92ee8df941e0471450bbab
revloc02 <4457735+revloc02@users.noreply.github.com>
2022-01-25 08:14:12 -0700

    adding AWS sns test

---
+++ src/test/java/forest/colver/datatransfer/it/SnsIntegrationTests.java
@@ -0,0 +1,22 @@
+package forest.colver.datatransfer.it;
+
+import static forest.colver.datatransfer.aws.SnsOperations.getSnsTopicAttributes;
+import static forest.colver.datatransfer.aws.Utils.getSbCreds;
+
+import org.junit.jupiter.api.Test;
+
+public class SnsIntegrationTests {
+
+  /**
+   * This tests a very specific use case, access from Enterprise Sandbox account to a topic created
+   * on my personal sandbox account. The topic was created with the EIS SNS module and used
+   * variables defined there to grant this specific access.
+   */
+  @Test
+  public void testGetSnsTopicAttributes() {
+    // place some messages
+    var creds = getSbCreds();
+    var topicArn = "arn:aws:sns:us-east-1:308660710587:topic_a"; // personal sandbox topic
+    getSnsTopicAttributes(creds, topicArn);
+  }
+}
