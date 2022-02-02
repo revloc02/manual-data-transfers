6c1ef0b2c0c162800c81f2bb92a6f040dd0d75c6
revloc02 <4457735+revloc02@users.noreply.github.com>
2022-02-02 07:46:16 -0700

    adding lambda test

---
+++ src/test/java/forest/colver/datatransfer/it/AwsLambdaIntTests.java
@@ -0,0 +1,72 @@
+package forest.colver.datatransfer.it;
+
+import static forest.colver.datatransfer.aws.LambdaOps.lambdaInvoke;
+import static forest.colver.datatransfer.aws.S3Operations.s3Delete;
+import static forest.colver.datatransfer.aws.S3Operations.s3Put;
+import static forest.colver.datatransfer.aws.SqsOperations.sqsDelete;
+import static forest.colver.datatransfer.aws.SqsOperations.sqsGet;
+import static forest.colver.datatransfer.aws.SqsOperations.sqsPurge;
+import static forest.colver.datatransfer.aws.Utils.getS3SourceCache;
+import static forest.colver.datatransfer.aws.Utils.getS3TargetCustomer;
+import static forest.colver.datatransfer.aws.Utils.getSbCreds;
+import static forest.colver.datatransfer.aws.Utils.getSqs1;
+import static forest.colver.datatransfer.config.Utils.readFile;
+import static forest.colver.datatransfer.config.Utils.sleepo;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Integration Tests for AWS Lambda
+ */
+public class AwsLambdaIntTests {
+
+  private static final Logger LOG = LoggerFactory.getLogger(AwsLambdaIntTests.class);
+  private static final String SANDBOX_SFTP_SOURCE_CAHCE_BUCKET = getS3SourceCache();
+
+  /**
+   * Currently creds to run this are obtained with: aws-azure-login --mode=gui --profile
+   * enterprise-sb
+   */
+  @Test
+  public void testInvokeLambda() {
+    var creds = getSbCreds();
+
+    // Place object in the s3 cache.
+    var objectKey = "ext-aiko1/outbound/dev/flox/dd/1test.txt";
+    var contents = readFile("src/main/resources/1test.txt", StandardCharsets.UTF_8);
+    s3Put(creds, SANDBOX_SFTP_SOURCE_CAHCE_BUCKET, objectKey, contents);
+
+    // The initial placement triggered the bridge lambda, so verify that...
+    var queue = getSqs1();
+    var messageResp = sqsGet(creds, queue);
+    assertThat(messageResp.hasMessages()).isTrue();
+    assertThat(messageResp.messages().get(0).body()).contains(
+        "\"key\": \"ext-aiko1/outbound/dev/flox/dd/1test.txt\"");
+
+    // ...and then clean it up.
+    sqsPurge(creds, queue);
+
+    // Now invoke the BridgeLambda on the object...
+    var function = "sftpBridgeLambda";
+    var payload = readFile("src/test/resources/invokeLambdaReq.json", StandardCharsets.UTF_8);
+    var response = lambdaInvoke(creds, function, payload);
+    assertThat(response.statusCode()).isEqualTo(200);
+
+    sleepo(1000);
+
+    // ...and ensure the SQS got the new message.
+    messageResp = sqsGet(creds, queue);
+    assertThat(messageResp.hasMessages()).isTrue();
+    assertThat(messageResp.messages().get(0).body()).contains(
+        "\"key\": \"ext-aiko1/outbound/dev/flox/dd/1test.txt\"");
+
+    // Cleanup the queue and the s3.
+    sqsDelete(creds, messageResp, queue);
+    s3Delete(creds, SANDBOX_SFTP_SOURCE_CAHCE_BUCKET, objectKey);
+  }
+
+}
