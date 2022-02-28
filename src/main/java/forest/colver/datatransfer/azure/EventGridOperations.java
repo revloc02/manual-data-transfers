24b049988fdbe5c969a6f9d850252145e6206e0c
revloc02 <4457735+revloc02@users.noreply.github.com>
2022-02-28 08:22:35 -0700

    adding azure event grid ops

---
+++ src/main/java/forest/colver/datatransfer/azure/EventGridOperations.java
@@ -0,0 +1,32 @@
+package forest.colver.datatransfer.azure;
+
+import com.azure.core.credential.AzureKeyCredential;
+import com.azure.core.util.BinaryData;
+import com.azure.messaging.eventgrid.EventGridPublisherClientBuilder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EventGridOperations {
+
+  private static final Logger LOG = LoggerFactory.getLogger(EventGridOperations.class);
+
+  /**
+   * Azure Event Grid Send Message
+   *
+   * @param eventGridHost The host name of the topic, e.g. topic1.westus2-1.eventgrid.azure.net
+   * @param eventGridTopicKey The event-grid-topic key
+   * @param events a map of event properties to send to the Event Grid
+   */
+  public static void aegSendMessage(String eventGridHost, String eventGridTopicKey,
+      List<BinaryData> events) {
+    var client2 = new EventGridPublisherClientBuilder()
+        .endpoint(eventGridHost)
+        .credential(new AzureKeyCredential(eventGridTopicKey))
+        .buildCustomEventPublisherClient();
+    client2.sendEvents(events);
+  }
+
+}
---
+++ src/test/java/forest/colver/datatransfer/it/AzureEventGridTests.java
@@ -0,0 +1,59 @@
+package forest.colver.datatransfer.it;
+
+//import static lds.emx.mdt.azure.EventGridOperations.receiveMessage;
+
+import static forest.colver.datatransfer.azure.EventGridOperations.aegSendMessage;
+import static forest.colver.datatransfer.azure.ServiceBusOperations.asbConsume;
+import static forest.colver.datatransfer.azure.ServiceBusOperations.connect;
+import static forest.colver.datatransfer.azure.Utils.EMX_SANDBOX_EVENTGRID_HOST;
+import static forest.colver.datatransfer.azure.Utils.EMX_SANDBOX_EVENTGRID_TOPIC_KEY;
+import static forest.colver.datatransfer.azure.Utils.EMX_SANDBOX_TEST_NAMESPACE;
+import static forest.colver.datatransfer.azure.Utils.EMX_SANDBOX_EVENTGRID_SUBSCRIPTION_QUEUE;
+import static forest.colver.datatransfer.azure.Utils.EMX_SANDBOX_NAMESPACE_SHARED_ACCESS_KEY;
+import static forest.colver.datatransfer.azure.Utils.EMX_SANDBOX_NAMESPACE_SHARED_ACCESS_POLICY;
+import static forest.colver.datatransfer.azure.Utils.createEvent;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.HashMap;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AzureEventGridTests {
+
+  private static final Logger LOG = LoggerFactory.getLogger(AzureEventGridTests.class);
+  private static final String EVENTGRID_HOST = EMX_SANDBOX_EVENTGRID_HOST;
+  private static final String EVENTGRID_TOPIC_KEY = EMX_SANDBOX_EVENTGRID_TOPIC_KEY;
+
+  /**
+   * Currently this test requires a sandbox account with resources allocated from
+   * devops/terraform/azure/main.tf
+   */
+  @Test
+  public void testSend() throws IOException {
+    var id = "test";
+    var subject = "AppEventA";
+    var body = "this is the body";
+    var eventType = "test.event.type";
+    var dataVersion = "0.0.1";
+    // todo: currently the timestamp is a hack, not sure how to fix that yet
+    aegSendMessage(EVENTGRID_HOST, EVENTGRID_TOPIC_KEY, createEvent(id, subject, body, eventType,
+        "2017-08-10T21:03:07+00:00", dataVersion));
+
+    // asb queue is subscribed to event grid, retrieve the message from the queue and check it
+    var msg = asbConsume(
+        connect(EMX_SANDBOX_TEST_NAMESPACE, EMX_SANDBOX_EVENTGRID_SUBSCRIPTION_QUEUE,
+            EMX_SANDBOX_NAMESPACE_SHARED_ACCESS_POLICY,
+            EMX_SANDBOX_NAMESPACE_SHARED_ACCESS_KEY));
+    LOG.info("message body type: {}", msg.getMessageBody().getBodyType().name());
+    var message =
+        new ObjectMapper()
+            .readValue(msg.getMessageBody().getBinaryData().get(0), HashMap.class);
+    assertEquals(body, message.get("data"));
+    assertEquals(eventType, message.get("eventType"));
+    assertEquals(subject, message.get("subject"));
+  }
+
+}
