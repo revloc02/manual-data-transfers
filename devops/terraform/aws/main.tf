21284a3f3961841a472881e27816a422e627b3d9
revloc02 <4457735+revloc02@users.noreply.github.com>
2022-03-16 07:41:13 -0600

    adding SNS and an SQS subscription to it

--- devops/terraform/aws/main.tf
+++ devops/terraform/aws/main.tf
@@ -1,7 +1,7 @@
 // simple topic for testGetSnsTopicAttributes int test
 module "basic_topic" {
   source            = "app.terraform.io/ICS/sns-topic/aws"
-  version           = "1.5.0"
+  version           = "~>1.0"
   name              = "topic_a"
   kms_master_key_id = "alias/aws/sns"
 
@@ -16,4 +16,26 @@ module "basic_topic" {
       }
     )
   ]
+}
+
+// SQS subscribed to the SNS
+module "my_queue" {
+  source                        = "app.terraform.io/ICS/sqs/aws"
+  version                       = "~>1.0"
+  name                          = "sub_demo_adv_queue"
+  kms_master_key_id             = module.my_key.arn
+  policy_allow_source_arns_send = [module.basic_topic.arn]
+}
+module "my_key" {
+  source                = "app.terraform.io/ICS/kms/aws"
+  version               = "~>1.0"
+  description           = "sub_demo_adv_key"
+  policy_allow_services = ["sns.amazonaws.com"]
+}
+module "simple_sub" {
+  source    = "app.terraform.io/ICS/sns-subscription/aws"
+  version   = "~>1.0"
+  topic_arn = module.basic_topic.arn
+  protocol  = "sqs"
+  endpoint  = module.my_queue.arn
 }
