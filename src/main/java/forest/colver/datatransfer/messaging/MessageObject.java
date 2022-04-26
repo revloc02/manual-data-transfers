1e7876fb580306ba7ec63933899f3105f4442f27
revloc02 <4457735+revloc02@users.noreply.github.com>
2022-04-26 07:40:13 -0600

    more work on the MessageObject

--- src/main/java/forest/colver/datatransfer/messaging/MessageObject.java
+++ src/main/java/forest/colver/datatransfer/messaging/MessageObject.java
@@ -1,16 +1,37 @@
 package forest.colver.datatransfer.messaging;
 
 import java.util.Map;
+import javax.jms.BytesMessage;
+import javax.jms.MapMessage;
 import javax.jms.Message;
+import javax.jms.ObjectMessage;
+import javax.jms.StreamMessage;
+import javax.jms.TextMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MessageObject {
+
+  private static final Logger LOG = LoggerFactory.getLogger(MessageObject.class);
 
   private Message message;
   private String payload;
   private Map<String, String> headers;
 
   public MessageObject(Message message) {
-    this.message = message;
+    if (message != null) {
+      this.message = message;
+      if (message instanceof TextMessage) {
+      } else if (message instanceof BytesMessage) {
+      } else if (message instanceof ObjectMessage) {
+      } else if (message instanceof StreamMessage) {
+      } else if (message instanceof MapMessage) {
+      } else {
+        LOG.info("Message Type: Unknown\n");
+      }
+    } else {
+      LOG.info("Message is null.");
+    }
   }
 
   public void displayMessage() {
