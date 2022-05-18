94751d44e357fb124110e4f96ad976fc46634224
revloc02 <4457735+revloc02@users.noreply.github.com>
2022-05-18 07:52:58 -0600

    CloudWatch Logs operations, lots of refactoring and making it consistent with the rest of my code

--- src/main/java/forest/colver/datatransfer/aws/CloudWatchLogsOps.java
+++ src/main/java/forest/colver/datatransfer/aws/CloudWatchLogsOps.java
@@ -1,9 +1,13 @@
 package forest.colver.datatransfer.aws;
 
+import static forest.colver.datatransfer.aws.Utils.getCloudWatchLogsClient;
+
 import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient;
+import software.amazon.awssdk.services.cloudwatchlogs.model.CreateLogStreamRequest;
 import software.amazon.awssdk.services.cloudwatchlogs.model.DescribeLogStreamsRequest;
 import software.amazon.awssdk.services.cloudwatchlogs.model.DescribeLogStreamsResponse;
 import software.amazon.awssdk.services.cloudwatchlogs.model.InputLogEvent;
@@ -13,24 +17,18 @@ import software.amazon.awssdk.services.cloudwatchlogs.model.PutLogEventsResponse
 public class CloudWatchLogsOps {
 
   private static final Logger LOG = LoggerFactory.getLogger(CloudWatchLogsOps.class);
-  public static PutLogEventsResponse putCWLogEvents(CloudWatchLogsClient logsClient, String logGroupName,
-      String streamName) {
 
-//    try {
-      DescribeLogStreamsRequest logStreamRequest = DescribeLogStreamsRequest.builder()
-          .logGroupName(logGroupName)
-          .logStreamNamePrefix(streamName)
-          .build();
-      DescribeLogStreamsResponse describeLogStreamsResponse = logsClient.describeLogStreams(
-          logStreamRequest);
-
-      LOG.info("describeLogStreamsResponse=", describeLogStreamsResponse.toString());
-      // Assume that a single stream is returned since a specific stream name was specified in the previous request.
-      String sequenceToken = describeLogStreamsResponse.logStreams().get(0).uploadSequenceToken();
+  public static PutLogEventsResponse putCWLogEvents(AwsCredentialsProvider awsCp,
+      String logGroupName,
+      String streamName, String message) {
+    PutLogEventsResponse response;
+    try (var logsClient = getCloudWatchLogsClient(awsCp)) {
+      createLogStream(logsClient, logGroupName, streamName);
+      var sequenceToken = getSequenceToken(logsClient, logGroupName, streamName);
 
       // Build an input log message to put to CloudWatch.
       InputLogEvent inputLogEvent = InputLogEvent.builder()
-          .message("{ \"key1\": \"value1\", \"key2\": \"value2\" }")
+          .message(message)
           .timestamp(System.currentTimeMillis())
           .build();
 
@@ -44,13 +42,32 @@ public class CloudWatchLogsOps {
           .sequenceToken(sequenceToken)
           .build();
 
-      var response = logsClient.putLogEvents(putLogEventsRequest);
+      response = logsClient.putLogEvents(putLogEventsRequest);
       LOG.info("Successfully put CloudWatch log event");
-
-//    } catch (CloudWatchException e) {
-//      System.err.println(e.awsErrorDetails().errorMessage());
-//      System.exit(1);
-//    }
+    }
     return response;
+  }
+
+  private static String getSequenceToken(CloudWatchLogsClient logsClient, String logGroupName,
+      String streamName) {
+    DescribeLogStreamsRequest logStreamRequest = DescribeLogStreamsRequest.builder()
+        .logGroupName(logGroupName)
+        .logStreamNamePrefix(streamName)
+        .build();
+    DescribeLogStreamsResponse describeLogStreamsResponse = logsClient.describeLogStreams(
+        logStreamRequest);
+
+    LOG.info("describeLogStreamsResponse=", describeLogStreamsResponse.toString());
+    // Assume that a single stream is returned since a specific stream name was specified in the previous request.
+    String sequenceToken = describeLogStreamsResponse.logStreams().get(0).uploadSequenceToken();
+    return sequenceToken;
+  }
+
+  private static void createLogStream(CloudWatchLogsClient logsClient, String logGroupName,
+      String streamName) {
+    CreateLogStreamRequest createLogStreamRequest = CreateLogStreamRequest.builder().logGroupName(
+        logGroupName).logStreamName(streamName).build();
+    var createLogStreamResponse = logsClient.createLogStream(createLogStreamRequest);
+    LOG.info("createLogStreamResponse=", createLogStreamResponse.sdkHttpResponse().statusCode());
   }
 }
--- src/test/java/forest/colver/datatransfer/it/AwsCloudWatchLogsIntTests.java
+++ src/test/java/forest/colver/datatransfer/it/AwsCloudWatchLogsIntTests.java
@@ -1,30 +1,24 @@
 package forest.colver.datatransfer.it;
 
 import static forest.colver.datatransfer.aws.CloudWatchLogsOps.putCWLogEvents;
-import static forest.colver.datatransfer.aws.Utils.getCloudWatchLogsClient;
 import static forest.colver.datatransfer.aws.Utils.getPrsnlSbCreds;
 
-import forest.colver.datatransfer.aws.CloudWatchLogsOps;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient;
-import software.amazon.awssdk.services.cloudwatchlogs.model.CreateLogStreamRequest;
 
 public class AwsCloudWatchLogsIntTests {
+
   private static final Logger LOG = LoggerFactory.getLogger(AwsCloudWatchLogsIntTests.class);
 
   @Test
   public void testCloudWatchLogs() {
     var logGroupName = "archive_test";
-    var streamName = "stream1235";
+    var streamName = "stream1236";
+    var message = "This is the log message.";
+    var anotherMessage = "{ \"key1\": \"value1\", \"key2\": \"value2\" }";
     var creds = getPrsnlSbCreds();
-    CloudWatchLogsClient logsClient = getCloudWatchLogsClient(creds);
-    CreateLogStreamRequest createLogStreamRequest = CreateLogStreamRequest.builder().logGroupName(logGroupName).logStreamName(streamName).build();
-    var createLogStreamResponse = logsClient.createLogStream(createLogStreamRequest);
-    LOG.info("createLogStreamResponse=", createLogStreamResponse.sdkHttpResponse().statusCode());
 
-    putCWLogEvents(logsClient, logGroupName, streamName) ;
-    logsClient.close(); //todo: just wondering, do I close my other aws clients for other aws resources I am connecting to in other code?
+    putCWLogEvents(creds, logGroupName, streamName, message);
   }
 }
