af13ea97326b143edf622522cd0c903418e8bae0
revloc02 <4457735+revloc02@users.noreply.github.com>
2022-07-06 09:57:56 -0600

    adding a move message from Qpid to SQS

--- src/test/java/forest/colver/datatransfer/it/MessagingIntTests.java
+++ src/test/java/forest/colver/datatransfer/it/MessagingIntTests.java
@@ -1,5 +1,9 @@
 package forest.colver.datatransfer.it;
 
+import static forest.colver.datatransfer.aws.SqsOperations.sqsDelete;
+import static forest.colver.datatransfer.aws.SqsOperations.sqsGet;
+import static forest.colver.datatransfer.aws.Utils.EMX_SANDBOX_TEST_SQS1;
+import static forest.colver.datatransfer.aws.Utils.getEmxSbCreds;
 import static forest.colver.datatransfer.config.Utils.generateUniqueStrings;
 import static forest.colver.datatransfer.config.Utils.getDefaultPayload;
 import static forest.colver.datatransfer.config.Utils.getTimeStamp;
@@ -31,6 +35,7 @@ import static forest.colver.datatransfer.messaging.JmsSend.sendMultipleSameMessa
 import static forest.colver.datatransfer.messaging.JmsSend.sendMultipleUniqueMessages;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import forest.colver.datatransfer.Main;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -502,6 +507,32 @@ public class MessagingIntTests {
       LOG.info("removed {}", future.get().getText());
     }
     assertThat(uuids.size()).isEqualTo(0);
+  }
+
+  /**
+   * This tests moving a message from Qpid to AWS SQS.
+   */
+  @Test
+  public void testMoveJmsToSqs() {
+    var payload = "this is the payload";
+    var messageProps = Map.of("timestamp", getTimeStamp(), "key2", "value2", "key3", "value3");
+    var queueName = "forest-test";
+    sendMessageAutoAck(STAGE, queueName, createTextMessage(payload, messageProps));
+
+    var creds = getEmxSbCreds();
+    Main.moveJmsToSqs(STAGE, queueName, creds, EMX_SANDBOX_TEST_SQS1);
+
+    // check that it arrived
+    var response = sqsGet(creds, EMX_SANDBOX_TEST_SQS1);
+    assertThat(response.messages().get(0).body()).isEqualTo(payload);
+    assertThat(response.messages().get(0).hasMessageAttributes()).isEqualTo(true);
+    assertThat(response.messages().get(0).messageAttributes().get("key2").stringValue()).isEqualTo(
+        "value2");
+    assertThat(response.messages().get(0).messageAttributes().get("key3").stringValue()).isEqualTo(
+        "value3");
+
+    // cleanup
+    sqsDelete(creds, response, EMX_SANDBOX_TEST_SQS1);
   }
 
 }
