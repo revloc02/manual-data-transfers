ebd431a6b59fa5d18eb2ae6750e111770ff3271f
revloc02 <4457735+revloc02@users.noreply.github.com>
2022-07-20 08:53:44 -0600

    revving upper end versions

--- devops/terraform/aws/providers.tf
+++ devops/terraform/aws/providers.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">=2.0,<4.0"
+      version = ">=2.0,<5.0"
     }
   }
-  required_version = ">= 0.14.11,< 1.1"
+  required_version = ">= 0.14.11,< 1.2"
 }
 provider "aws" {
   region                  = "us-east-1"
