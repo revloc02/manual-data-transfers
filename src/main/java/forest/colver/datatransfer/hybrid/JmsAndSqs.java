daa3896eb8fb7467336d4c8806f70f1181e05654
revloc02 <4457735+revloc02@users.noreply.github.com>
2022-07-20 08:50:54 -0600

    starting a move specific messages jms to sqs method

--- src/main/java/forest/colver/datatransfer/hybrid/JmsAndSqs.java
+++ src/main/java/forest/colver/datatransfer/hybrid/JmsAndSqs.java
@@ -2,16 +2,21 @@ package forest.colver.datatransfer.hybrid;
 
 import static forest.colver.datatransfer.aws.SqsOperations.sqsConsume;
 import static forest.colver.datatransfer.aws.SqsOperations.sqsSend;
+import static forest.colver.datatransfer.config.Utils.getPassword;
+import static forest.colver.datatransfer.config.Utils.getUsername;
 import static forest.colver.datatransfer.messaging.JmsConsume.consumeOneMessage;
 import static forest.colver.datatransfer.messaging.JmsSend.createTextMessage;
 import static forest.colver.datatransfer.messaging.JmsSend.sendMessageAutoAck;
+import static javax.jms.JMSContext.CLIENT_ACKNOWLEDGE;
 
 import forest.colver.datatransfer.messaging.Environment;
 import java.util.Enumeration;
 import java.util.Map;
 import java.util.Map.Entry;
 import javax.jms.JMSException;
+import javax.jms.Message;
 import javax.jms.TextMessage;
+import org.apache.qpid.jms.JmsConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
@@ -32,7 +37,7 @@ public class JmsAndSqs {
    * @param awsCreds Credentials for the AWS environment.
    * @param sqs The AWS SQS.
    */
-  public static void moveJmsToSqs(Environment qpidEnv, String qpidQ,
+  public static void moveOneJmsToSqs(Environment qpidEnv, String qpidQ,
       AwsCredentialsProvider awsCreds, String sqs) {
     TextMessage msg = (TextMessage) consumeOneMessage(qpidEnv, qpidQ);
     var payload = "";
@@ -51,7 +56,7 @@ public class JmsAndSqs {
     sqsSend(awsCreds, sqs, payload, properties);
   }
 
-  public static void moveSqsToJms(AwsCredentialsProvider awsCreds, String sqs, Environment env,
+  public static void moveOneSqsToJms(AwsCredentialsProvider awsCreds, String sqs, Environment env,
       String queue) {
     var receiveMessageResponse = sqsConsume(awsCreds, sqs);
     var payload = receiveMessageResponse.messages().get(0).body();
@@ -62,6 +67,40 @@ public class JmsAndSqs {
     }
     var message = createTextMessage(payload, properties);
     sendMessageAutoAck(env, queue, message);
+  }
+
+  // todo: this.
+  public static void moveAllSpecificMessagesFromJmsToSqs(Environment env, String queue, String selector, AwsCredentialsProvider awsCreds, String sqs) {
+    var cf = new JmsConnectionFactory(env.url());
+    try (var ctx = cf.createContext(getUsername(), getPassword(), CLIENT_ACKNOWLEDGE)) {
+      var fromQ = ctx.createQueue(queue);
+      var counter = 0;
+      try (var consumer = ctx.createConsumer(fromQ, selector)) {
+        var moreMessages = true;
+//        var toQ = ctx.createQueue(toQueueName);
+        Message message;
+        while (moreMessages) {
+          message = consumer.receive(2_000L);
+          if (message != null) {
+            counter++;
+//            ctx.createProducer().send(toQ, message);
+            message.acknowledge();
+            LOG.info(
+                "Moved from Queue={}:{} to Queue={}:{}, counter={}",
+                env.name(),
+                queue,
+                env.name(),
+//                toQueueName,
+                counter);
+          } else {
+            moreMessages = false;
+          }
+        }
+      } catch (JMSException e) {
+        e.printStackTrace();
+      }
+      LOG.info("Moved {} messages for selector={}.", counter, selector);
+    }
   }
   //todo: probably should have a move all, and a move specific
 }
