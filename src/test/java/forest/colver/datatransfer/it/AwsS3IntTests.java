62f8ebb0da919672dad0047ec47b122629548aba
revloc02 <4457735+revloc02@users.noreply.github.com>
2022-09-02 14:00:27 -0600

    some renames and adding a couple unit tests

--- src/test/java/forest/colver/datatransfer/it/AwsS3IntTests.java
+++ src/test/java/forest/colver/datatransfer/it/AwsS3IntTests.java
@@ -2,6 +2,7 @@ package forest.colver.datatransfer.it;
 
 import static forest.colver.datatransfer.aws.S3Operations.s3Copy;
 import static forest.colver.datatransfer.aws.S3Operations.s3Delete;
+import static forest.colver.datatransfer.aws.S3Operations.s3Head;
 import static forest.colver.datatransfer.aws.S3Operations.s3List;
 import static forest.colver.datatransfer.aws.S3Operations.s3Put;
 import static forest.colver.datatransfer.aws.Utils.S3_INTERNAL;
@@ -10,9 +11,11 @@ import static forest.colver.datatransfer.aws.Utils.getEmxSbCreds;
 import static forest.colver.datatransfer.config.Utils.getDefaultPayload;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 /**
  * Integration Tests for AWS S3
@@ -25,17 +28,17 @@ public class AwsS3IntTests {
   public void testS3Copy() {
     // place a file
     var creds = getEmxSbCreds();
-    var sourceKey = "revloc02/source/test/test.txt";
-    s3Put(creds, S3_INTERNAL, sourceKey, getDefaultPayload());
+    var objectKey = "revloc02/source/test/test.txt";
+    s3Put(creds, S3_INTERNAL, objectKey, getDefaultPayload());
 
     // verify the file is in the source
-    var objects = s3List(creds, S3_INTERNAL, sourceKey);
+    var objects = s3List(creds, S3_INTERNAL, objectKey);
     assertThat(objects.size()).isEqualTo(1);
-    assertThat(objects.get(0).key()).isEqualTo(sourceKey);
+    assertThat(objects.get(0).key()).isEqualTo(objectKey);
 
     // copy file
     var destKey = "blake/inbound/dev/some-bank/ack/testCopied.txt";
-    s3Copy(creds, S3_INTERNAL, sourceKey, S3_TARGET_CUSTOMER, destKey);
+    s3Copy(creds, S3_INTERNAL, objectKey, S3_TARGET_CUSTOMER, destKey);
 
     // verify the copy
     objects = s3List(creds, S3_TARGET_CUSTOMER, destKey);
@@ -46,38 +49,92 @@ public class AwsS3IntTests {
 //    s3Get(creds, SANDBOX_SFTP_TARGET_CUSTOMER_BUCKET, destKey);
 
     // delete the files
-    s3Delete(creds, S3_INTERNAL, sourceKey);
+    s3Delete(creds, S3_INTERNAL, objectKey);
     s3Delete(creds, S3_TARGET_CUSTOMER, destKey);
+  }
+
+  @Test
+  public void testS3PutObjectRequest() {
+    // put a file
+    var objectKey = "revloc02/source/test/test.txt";
+    var creds = getEmxSbCreds();
+    var putObjectRequest = PutObjectRequest.builder()
+        .bucket(S3_INTERNAL)
+        .key(objectKey)
+        .build();
+    s3Put(creds, getDefaultPayload(), putObjectRequest);
+
+    // verify the file is there
+    var objects = s3List(creds, S3_INTERNAL, objectKey);
+    assertThat(objects.size()).isEqualTo(1);
+    assertThat(objects.get(0).key()).isEqualTo(objectKey);
+
+    // delete the file
+    s3Delete(creds, S3_INTERNAL, objectKey);
+
+    // verify the file is gone
+    objects = s3List(creds, S3_INTERNAL, objectKey);
+    assertThat(objects.size()).isEqualTo(0);
+  }
+
+  @Test
+  public void testS3Head() {
+    // put a file
+    var objectKey = "revloc02/source/test/test.txt";
+    var creds = getEmxSbCreds();
+    var metadata = Map.of("key", "value");
+    var putObjectRequest = PutObjectRequest.builder()
+        .bucket(S3_INTERNAL)
+        .key(objectKey)
+        .metadata(metadata)
+        .build();
+    s3Put(creds, getDefaultPayload(), putObjectRequest);
+
+    // verify the file is there
+    var objects = s3List(creds, S3_INTERNAL, objectKey);
+    assertThat(objects.size()).isEqualTo(1);
+    assertThat(objects.get(0).key()).isEqualTo(objectKey);
+
+    // use head to verify metadata
+    var headObjectResponse = s3Head(creds, S3_INTERNAL, objectKey);
+    assertThat(headObjectResponse.metadata().get("key")).isEqualTo("value");
+
+    // delete the file
+    s3Delete(creds, S3_INTERNAL, objectKey);
+
+    // verify the file is gone
+    objects = s3List(creds, S3_INTERNAL, objectKey);
+    assertThat(objects.size()).isEqualTo(0);
   }
 
   @Test
   public void testS3Delete() {
     // put a file
-    var sourceKey = "revloc02/source/test/test.txt";
+    var objectKey = "revloc02/source/test/test.txt";
     var creds = getEmxSbCreds();
-    s3Put(creds, S3_INTERNAL, sourceKey, getDefaultPayload());
+    s3Put(creds, S3_INTERNAL, objectKey, getDefaultPayload());
 
     // verify the file is there
-    var objects = s3List(creds, S3_INTERNAL, sourceKey);
+    var objects = s3List(creds, S3_INTERNAL, objectKey);
     assertThat(objects.size()).isEqualTo(1);
-    assertThat(objects.get(0).key()).isEqualTo(sourceKey);
+    assertThat(objects.get(0).key()).isEqualTo(objectKey);
 
     // delete the file
-    s3Delete(creds, S3_INTERNAL, sourceKey);
+    s3Delete(creds, S3_INTERNAL, objectKey);
 
     // verify the file is gone
-    objects = s3List(creds, S3_INTERNAL, sourceKey);
+    objects = s3List(creds, S3_INTERNAL, objectKey);
     assertThat(objects.size()).isEqualTo(0);
   }
 
   @Test
   public void testS3List() {
     var creds = getEmxSbCreds();
-    var key = "revloc02/target/test/mdtTest1.txt";
-    s3Put(creds, S3_INTERNAL, key, getDefaultPayload());
+    var objectKey = "revloc02/target/test/mdtTest1.txt";
+    s3Put(creds, S3_INTERNAL, objectKey, getDefaultPayload());
     var objects = s3List(creds, S3_INTERNAL, "revloc02/target/test");
-    assertThat(objects.get(1).key()).isEqualTo(key);
+    assertThat(objects.get(1).key()).isEqualTo(objectKey);
     assertThat(objects.get(1).size()).isEqualTo(40L);
-    s3Delete(creds, S3_INTERNAL, key);
+    s3Delete(creds, S3_INTERNAL, objectKey);
   }
 }
