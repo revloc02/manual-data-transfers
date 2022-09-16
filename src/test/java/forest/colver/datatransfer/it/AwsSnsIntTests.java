7725f8ccc650a3b7cc4f4cc4a56be45fd377d1f7
revloc02 <4457735+revloc02@users.noreply.github.com>
2022-09-16 10:16:24 -0600

    clean up comments and use awaitility

--- src/test/java/forest/colver/datatransfer/it/AwsSnsIntTests.java
+++ src/test/java/forest/colver/datatransfer/it/AwsSnsIntTests.java
@@ -3,6 +3,7 @@ package forest.colver.datatransfer.it;
 import static forest.colver.datatransfer.aws.SnsOperations.getSnsTopicAttributes;
 import static forest.colver.datatransfer.aws.SnsOperations.publishTopic;
 import static forest.colver.datatransfer.aws.SqsOperations.sqsDelete;
+import static forest.colver.datatransfer.aws.SqsOperations.sqsDepth;
 import static forest.colver.datatransfer.aws.SqsOperations.sqsReadOneMessage;
 import static forest.colver.datatransfer.aws.Utils.PERSONAL_SANDBOX_SQS_SUB_SNS;
 import static forest.colver.datatransfer.aws.Utils.PERSONAL_SANDBOX_TEST_SNS_TOPIC_ARN;
@@ -10,7 +11,9 @@ import static forest.colver.datatransfer.aws.Utils.getEmxSbCreds;
 import static forest.colver.datatransfer.aws.Utils.getPrsnlSbCreds;
 import static forest.colver.datatransfer.config.Utils.pause;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 
+import java.time.Duration;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -32,11 +35,10 @@ public class AwsSnsIntTests {
    */
   @Test
   public void testGetSnsTopicAttributes() {
-    // personal sandbox SNS topic created from terraform/aws/main.tf
-    // refresh personal sandbox creds and then run that Terraform and copy ARN to here
-
-    // need to refresh Enterprise Sandbox creds to run
-    getSnsTopicAttributes(getEmxSbCreds(), SNS_ARN);
+    // refresh personal sandbox creds
+    // run TF in terraform/aws/main.tf to create personal sandbox SNS topic
+    // copy the created SNS ARN to .aws/credentials.properties
+    getSnsTopicAttributes(getPrsnlSbCreds(), SNS_ARN);
   }
 
   @Test
@@ -46,7 +48,11 @@ public class AwsSnsIntTests {
     var message = "testing SNS publish topic via SQS subscription";
     publishTopic(getPrsnlSbCreds(), SNS_ARN, message);
 
-    pause(2); // todo: see if awaitility will work here
+    await()
+        .pollInterval(Duration.ofSeconds(3))
+        .atMost(Duration.ofSeconds(60))
+        .until(() -> sqsDepth(creds, SQS) >= 1);
+
     // check that it arrived
     var response = sqsReadOneMessage(creds, SQS);
     LOG.info("response.messages().get(0).body():{}", response.messages().get(0).body());
