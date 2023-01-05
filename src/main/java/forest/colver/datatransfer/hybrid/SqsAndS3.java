b2170c438d0a4158eb58b76a10a12281e0170d3e
revloc02 <4457735+revloc02@users.noreply.github.com>
2023-01-05 10:53:26 -0700

    adding another method, initial commit

--- src/main/java/forest/colver/datatransfer/hybrid/SqsAndS3.java
+++ src/main/java/forest/colver/datatransfer/hybrid/SqsAndS3.java
@@ -2,6 +2,7 @@ package forest.colver.datatransfer.hybrid;
 
 import static forest.colver.datatransfer.aws.S3Operations.s3Put;
 import static forest.colver.datatransfer.aws.SqsOperations.sqsConsumeOneMessage;
+import static forest.colver.datatransfer.aws.SqsOperations.sqsReadOneMessage;
 import static forest.colver.datatransfer.aws.Utils.convertSqsMessageAttributesToStrings;
 
 import org.slf4j.Logger;
@@ -18,6 +19,23 @@ public class SqsAndS3 {
   public static void moveOneSqsToS3(
       AwsCredentialsProvider awsCreds, String sqs, String bucket, String objectKey) {
     var sqsMsg = sqsConsumeOneMessage(awsCreds, sqs);
+    if (sqsMsg != null) {
+      // send body and properties to s3
+      s3Put(awsCreds, bucket, objectKey, sqsMsg.body(),
+          convertSqsMessageAttributesToStrings(sqsMsg.messageAttributes()));
+    } else {
+      LOG.error("ERROR: SQS message was null.");
+    }
+  }
+
+  // todo: this needs a unit test
+  /**
+   * Copy (read) next message from an SQS and save it to an S3.
+   */
+  public static void copyOneSqsToS3(
+      AwsCredentialsProvider awsCreds, String sqs, String bucket, String objectKey) {
+    var response = sqsReadOneMessage(awsCreds, sqs);
+    var sqsMsg = response.messages().get(0);
     if (sqsMsg != null) {
       // send body and properties to s3
       s3Put(awsCreds, bucket, objectKey, sqsMsg.body(),
