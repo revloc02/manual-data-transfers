898ea68af5298ccbae77b57acdcec79665314bc8
revloc02 <4457735+revloc02@users.noreply.github.com>
2023-01-27 06:58:02 -0700

    unit test for read and write to disk

---
+++ src/test/java/forest/colver/datatransfer/UtilsTests.java
@@ -0,0 +1,24 @@
+package forest.colver.datatransfer;
+
+import static forest.colver.datatransfer.config.Utils.getDefaultPayload;
+import static forest.colver.datatransfer.config.Utils.readFile;
+import static forest.colver.datatransfer.config.Utils.writeFile;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for config Utils.
+ */
+public class UtilsTests {
+
+  @Test
+  public void testWriteAndReadFile() {
+    var path = "/Users/revloc02/Downloads/manual-data-transfers-test.txt";
+    var payload = getDefaultPayload().getBytes(StandardCharsets.UTF_8);
+    writeFile(path, payload);
+    var contents = readFile(path, StandardCharsets.UTF_8);
+    assertThat(contents).contains(getDefaultPayload());
+  }
+}
