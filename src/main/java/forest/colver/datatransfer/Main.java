de68604b9843eafe3a643b0d670b434b55e10bbe
revloc02 <4457735+revloc02@users.noreply.github.com>
2023-02-07 05:54:23 -0700

    commiting the next iteration of Main

--- src/main/java/forest/colver/datatransfer/Main.java
+++ src/main/java/forest/colver/datatransfer/Main.java
@@ -1,5 +1,6 @@
 package forest.colver.datatransfer;
 
+import static forest.colver.datatransfer.CommonTasks.cleanupSftpErrorSqsStage;
 import static forest.colver.datatransfer.aws.SqsOperations.*;
 import static forest.colver.datatransfer.aws.Utils.*;
 import static forest.colver.datatransfer.messaging.Environment.*;
@@ -16,27 +17,16 @@ public class Main {
   private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
   public static void main(String[] args) {
-    LOG.info("start");
+    LOG.info("main start");
 
-    // todo: are my retrieve methods appropriate so that I can get a message off of the EMX replay cache? What if there are multiple messages there that I need?
+    cleanupSftpErrorSqsStage();
+
 //    browseForSpecificMessage(PROD, "emx-replay-cache", "emxReplayPriorTrackingNumber='89cb031a-f47e-4fb0-8666-b586760fd5cd'");
-
-        moveAllSpecificMessages(STAGE, "emx-error", "emxSystem='crm-missionary-recommend'", "emx-to-crm-missionary-recommend-test");
-
-
-//    sqsCopyAll(getEmxSbCreds(), "sftp-error", "blake-emxonramp-dev");
-//    sqsMoveMessagesWithPayloadLike(getEmxSbCreds(), "sftp-error", "lifeflightTestFile", "blake-emxonramp-test");
-//    sqsMoveMessagesWithPayloadLike(getEmxNpCreds(), "sftp-error", "lifeflightTestFile", "test-queue");
-
 
 //    moveAllSpecificMessages(PROD, "emx-error", "emxSystem='si-services' AND emxSystemEnvironment='prod'", "emx-to-si-services-prod");
 //    moveAllSpecificMessages(STAGE, "emx-error", "emxSystem='si-services' AND emxSystemEnvironment='test'", "emx-to-si-services-test");
 //    moveAllSpecificMessages(STAGE, "emx-error", "emxSystem='si-services' AND emxSystemEnvironment='stage'", "emx-to-si-services-stage");
-//
-//    moveAllSpecificMessages(STAGE, "emx-error",
-//        "emxSystem='lenel'",
-//        "lenel-tyler");
-//
+
 //    moveAllSpecificMessages(PROD, "emx-error",
 //        "emxSystem='identity-vault' AND emxErrorMessage LIKE '%HttpTimeoutException: request timed out%'",
 //        "emx-to-identity-vault-prod");
