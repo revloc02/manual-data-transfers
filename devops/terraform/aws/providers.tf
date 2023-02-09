22bb9d4a4dd01fb8c18089950473db8ec977d3ab
revloc02 <4457735+revloc02@users.noreply.github.com>
2023-02-09 13:35:55 -0700

    updating TF version

--- devops/terraform/aws/providers.tf
+++ devops/terraform/aws/providers.tf
@@ -5,7 +5,7 @@ terraform {
       version = ">=2.0,<5.0"
     }
   }
-  required_version = ">= 0.14.11,< 1.2"
+  required_version = ">= 0.14.11,< 1.3"
 }
 provider "aws" {
   region                  = "us-east-1"
