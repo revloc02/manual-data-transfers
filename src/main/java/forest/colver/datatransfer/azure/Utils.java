626fad6113dc3d6f1a076b65e23425fa497e4995
revloc02 <4457735+revloc02@users.noreply.github.com>
2023-09-01 06:56:09 -0600

    adding more creds

--- src/main/java/forest/colver/datatransfer/azure/Utils.java
+++ src/main/java/forest/colver/datatransfer/azure/Utils.java
@@ -37,6 +37,8 @@ public class Utils {
   public static final String EMX_SANDBOX_EVENTGRID_HOST = userCreds.getProperty("azure-emx-sandbox-eventgrid-host");
   public static final String EMX_SANDBOX_EVENTGRID_TOPIC_KEY = userCreds.getProperty("azure-emx-sandbox-eventgrid-topic-key");
   public static final String EMX_SANDBOX_STORAGE_ACCOUNT_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=foresttestsa;AccountKey=" + userCreds.getProperty("azure-emx-sandbox-forest-sa-account-key") + ";EndpointSuffix=core.windows.net";
+  public static final String EMX_PROD_EMXPROD_STORAGE_ACCOUNT_CONNECTION_STRING = userCreds.getProperty("azure-emx-prod-emxprod-sa-account-key");
+  public static final String EMX_PROD_EMXPROD_EXT_EMCOR_SAS_TOKEN = userCreds.getProperty("azure-emxprod-sa-ext-emcor-prod-source-sas-token");
 
   public static IMessage createIMessage(String payload, String label, String id,
       Map<String, Object> properties) {
