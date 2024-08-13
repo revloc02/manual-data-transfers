f5207c40a59ecdcafe515161a0c178671ec3f492
revloc02 <4457735+revloc02@users.noreply.github.com>
2024-08-13 08:48:41 -0600

    formatting

--- src/main/java/forest/colver/datatransfer/azure/BlobStorageOperations.java
+++ src/main/java/forest/colver/datatransfer/azure/BlobStorageOperations.java
@@ -17,44 +17,48 @@ public class BlobStorageOperations {
     throw new UnsupportedOperationException("This is a utility class and cannot be instantiated.");
   }
 
-  public static void blobPut(String connectStr, String endpoint, String containerName,
-      String filename, String contents) {
-    BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
-        .connectionString(connectStr)
-        .endpoint(endpoint)
-        .buildClient();
+  public static void blobPut(
+      String connectStr, String endpoint, String containerName, String filename, String contents) {
+    BlobServiceClient blobServiceClient =
+        new BlobServiceClientBuilder()
+            .connectionString(connectStr)
+            .endpoint(endpoint)
+            .buildClient();
 
     try (var dataStream = new ByteArrayInputStream(contents.getBytes(StandardCharsets.UTF_8))) {
       var blobContainerClient = blobServiceClient.getBlobContainerClient(containerName);
-      blobContainerClient.getBlobClient(filename).getBlockBlobClient()
+      blobContainerClient
+          .getBlobClient(filename)
+          .getBlockBlobClient()
           .upload(dataStream, contents.length());
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
-  public static void blobPutSas(String sasToken, String endpoint, String containerName,
-      String filename, String contents) {
-    BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
-        .sasToken(sasToken)
-        .endpoint(endpoint)
-        .buildClient();
+  public static void blobPutSas(
+      String sasToken, String endpoint, String containerName, String filename, String contents) {
+    BlobServiceClient blobServiceClient =
+        new BlobServiceClientBuilder().sasToken(sasToken).endpoint(endpoint).buildClient();
 
     try (var dataStream = new ByteArrayInputStream(contents.getBytes(StandardCharsets.UTF_8))) {
       var blobContainerClient = blobServiceClient.getBlobContainerClient(containerName);
-      blobContainerClient.getBlobClient(filename).getBlockBlobClient()
+      blobContainerClient
+          .getBlobClient(filename)
+          .getBlockBlobClient()
           .upload(dataStream, contents.length());
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
-  public static ByteArrayOutputStream blobGet(String connectStr, String endpoint,
-      String containerName, String filename) {
-    BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
-        .connectionString(connectStr)
-        .endpoint(endpoint)
-        .buildClient();
+  public static ByteArrayOutputStream blobGet(
+      String connectStr, String endpoint, String containerName, String filename) {
+    BlobServiceClient blobServiceClient =
+        new BlobServiceClientBuilder()
+            .connectionString(connectStr)
+            .endpoint(endpoint)
+            .buildClient();
 
     ByteArrayOutputStream dataStream = new ByteArrayOutputStream();
     try (dataStream) {
@@ -76,13 +80,10 @@ public class BlobStorageOperations {
    * @param filename Filename.
    * @return An output stream of the file.
    */
-  public static ByteArrayOutputStream blobGetSas(String sasToken, String endpoint,
-      String containerName,
-      String filename) {
-    BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
-        .sasToken(sasToken)
-        .endpoint(endpoint)
-        .buildClient();
+  public static ByteArrayOutputStream blobGetSas(
+      String sasToken, String endpoint, String containerName, String filename) {
+    BlobServiceClient blobServiceClient =
+        new BlobServiceClientBuilder().sasToken(sasToken).endpoint(endpoint).buildClient();
 
     ByteArrayOutputStream dataStream = new ByteArrayOutputStream();
     try (dataStream) {
@@ -94,29 +95,27 @@ public class BlobStorageOperations {
     return dataStream;
   }
 
-  public static void blobDelete(String connectStr, String endpoint, String containerName,
-      String filename) {
-    BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
-        .connectionString(connectStr)
-        .endpoint(endpoint)
-        .buildClient();
+  public static void blobDelete(
+      String connectStr, String endpoint, String containerName, String filename) {
+    BlobServiceClient blobServiceClient =
+        new BlobServiceClientBuilder()
+            .connectionString(connectStr)
+            .endpoint(endpoint)
+            .buildClient();
 
     var blobContainerClient = blobServiceClient.getBlobContainerClient(containerName);
     blobContainerClient.getBlobClient(filename).delete();
     LOG.info("BLOBDELETE: The object {} was deleted from {}.", filename, containerName);
   }
 
-  public static void blobDeleteSas(String sasToken, String endpoint, String containerName,
-      String filename) {
-    BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
-        .sasToken(sasToken)
-        .endpoint(endpoint)
-        .buildClient();
+  public static void blobDeleteSas(
+      String sasToken, String endpoint, String containerName, String filename) {
+    BlobServiceClient blobServiceClient =
+        new BlobServiceClientBuilder().sasToken(sasToken).endpoint(endpoint).buildClient();
 
     var blobContainerClient = blobServiceClient.getBlobContainerClient(containerName);
     blobContainerClient.getBlobClient(filename).delete();
   }
 
   // todo: can I make move and copy methods between 2 blob containers?
-  // todo: can I research copying from one blob container to another without using Java?
 }
