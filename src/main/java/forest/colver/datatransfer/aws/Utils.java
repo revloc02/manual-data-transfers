b9ce66871156b7e0c8cbfe420e35565b74195082
revloc02 <4457735+revloc02@users.noreply.github.com>
2024-09-30 12:43:24 -0600

    cleanup

--- src/main/java/forest/colver/datatransfer/aws/Utils.java
+++ src/main/java/forest/colver/datatransfer/aws/Utils.java
@@ -148,8 +148,7 @@ public class Utils {
   public static void awsResponseValidation(AwsResponse response) {
     var responseCode = response.sdkHttpResponse().statusCode();
     if (responseCode >= 300) {
-      LOG.info(
-          "ERROR: {}, responseMetadata={}", responseCode, response.responseMetadata().toString());
+      LOG.info("ERROR: {}, responseMetadata={}", responseCode, response.responseMetadata());
       throw new IllegalStateException("Unsuccessful AWS request. Status Code: " + responseCode);
     }
   }
