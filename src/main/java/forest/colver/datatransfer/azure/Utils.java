75b3152877184488d4bd77d376599a23a36a88ff
revloc02 <4457735+revloc02@users.noreply.github.com>
2024-09-30 13:39:23 -0600

    updating the credentials and label for them

--- src/main/java/forest/colver/datatransfer/azure/Utils.java
+++ src/main/java/forest/colver/datatransfer/azure/Utils.java
@@ -43,7 +43,7 @@ public class Utils {
       userCreds.getProperty("azure-emx-sandbox-eventgrid-host");
   public static final String EMX_SANDBOX_EVENTGRID_TOPIC_KEY =
       userCreds.getProperty("azure-emx-sandbox-eventgrid-topic-key");
-  public static final String EMX_SANDBOX_SA_FOREST_CONN_STR =
+  public static final String EMX_SANDBOX_SA_CONN_STR =
       "DefaultEndpointsProtocol=https;AccountName=emxsandbox;AccountKey="
           + userCreds.getProperty("azure-emx-sandbox-emxsandbox-sa-account-key")
           + ";EndpointSuffix=core.windows.net";
@@ -51,10 +51,10 @@ public class Utils {
       userCreds.getProperty("azure-emx-prod-extemcorprod-sa-conn-str");
   public static final String EMX_EXTEMCORNP_SA_EXT_EMCOR_NP_SAS_TOKEN =
       userCreds.getProperty("azure-extemcornp-sa-ext-emcor-np-source-sas-token");
-  public static final String EMX_SANDBOX_SA_FOREST_FOREST_TEST_BLOB_SAS =
-      userCreds.getProperty("azure-emx-sandbox-foresttestsa-forest-test-blob-sas-token");
-  public static final String EMX_SANDBOX_SA_FOREST_FOREST_TEST_BLOB2_SAS =
-      userCreds.getProperty("azure-emx-sandbox-foresttestsa-forest-test-blob2-sas-token");
+  public static final String EMX_SANDBOX_SA_FOREST_TEST_BLOB_SAS =
+      userCreds.getProperty("azure-emx-sandbox-emxsandbox-sa-forest-test-blob-sas-token");
+  public static final String EMX_SANDBOX_SA_FOREST_TEST_BLOB2_SAS =
+      userCreds.getProperty("azure-emx-sandbox-emxsandbox-sa-forest-test-blob2-sas-token");
 
   public static IMessage createIMessage(
       String payload, String label, String id, Map<String, Object> properties) {
