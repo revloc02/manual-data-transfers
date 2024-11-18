55b12d165ab484b238dba065dcc5ba20da011624
revloc02 <4457735+revloc02@users.noreply.github.com>
2024-11-18 07:57:23 -0700

    adding ServiceBusOperations class

---
+++ src/main/java/forest/colver/datatransfer/azure/ServiceBusOperations.java
@@ -0,0 +1,12 @@
+package forest.colver.datatransfer.azure;
+
+/**
+ * Operations to interact with Azure Service Bus, both queue and topic. Currently, this is an
+ * experiment.
+ */
+public class ServiceBusOperations {
+  private ServiceBusOperations() {
+    // https://rules.sonarsource.com/java/RSPEC-1118/
+    throw new UnsupportedOperationException("This is a utility class and cannot be instantiated.");
+  }
+}
