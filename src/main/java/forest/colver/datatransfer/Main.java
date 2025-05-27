143274523db6d4337a9517ea53537bc10e281413
revloc02 <4457735+revloc02@users.noreply.github.com>
2025-05-27 06:07:08 -0600

    latest Main

--- src/main/java/forest/colver/datatransfer/Main.java
+++ src/main/java/forest/colver/datatransfer/Main.java
@@ -1,7 +1,8 @@
 package forest.colver.datatransfer;
 
-import static forest.colver.datatransfer.CommonTasks.cleanS3Directory;
-import static forest.colver.datatransfer.CommonTasks.examineS3Objects;
+import static forest.colver.datatransfer.messaging.Environment.PROD;
+import static forest.colver.datatransfer.messaging.Environment.STAGE;
+import static forest.colver.datatransfer.messaging.JmsConsume.moveAllSpecificMessages;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,28 +15,59 @@ public class Main {
   public static void main(String[] args) {
     LOG.info("main start");
 
-    var bucket = "cp-aws-gayedtiak3nflbiftucz-s3-logging";
-    var keyPrefix = "emx-sandbox-sftp/";
-    examineS3Objects(bucket, keyPrefix);
+    //    var bucket = "cp-aws-gayedtiak3nflbiftucz-s3-logging";
+    //    var keyPrefix = "emx-sandbox-sftp/";
+    //    examineS3Objects(bucket, keyPrefix);
 
+    //    cleanupSftpErrorSqsStage();
 
+    // si-services
+    moveAllSpecificMessages(
+        PROD,
+        "emx-error",
+        "emxSystem='si-services' AND emxSystemEnvironment='prod'",
+        "emx-to-si-services-prod");
+    moveAllSpecificMessages(
+        STAGE,
+        "emx-error",
+        "emxSystem='si-services' AND emxSystemEnvironment='test'",
+        "emx-to-si-services-test");
+    moveAllSpecificMessages(
+        STAGE,
+        "emx-error",
+        "emxSystem='si-services' AND emxSystemEnvironment='stage'",
+        "watchtower-si-services");
+    moveAllSpecificMessages(
+        STAGE,
+        "watchtower-si-services",
+        "emxSystem='si-services' AND emxSystemEnvironment='stage' AND emxErrorRampName='off-ramp'",
+        "emx-to-si-services-stage");
+    moveAllSpecificMessages(
+        STAGE,
+        "watchtower-si-services",
+        "emxSystem='si-services' AND emxSystemEnvironment='stage' AND emxErrorRampName='off-ramp-wds'",
+        "emx-to-si-services-stage-wds");
 
-//    cleanupSftpErrorSqsStage();
+    // Lenel
+    moveAllSpecificMessages(
+        PROD,
+        "emx-error",
+        "emxSystem='lenel' AND emxErrorRampName='hr-data-broker'",
+        "watchtower-lenel-hr-data-broker");
+    moveAllSpecificMessages(
+        PROD, "emx-error", "emxSystem='lenel' AND emxErrorRampName='off-ramp'", "watchtower-lenel");
+    moveAllSpecificMessages(
+        PROD,
+        "watchtower-lenel-hr-data-broker",
+        "emxSystem='lenel' AND emxErrorRampName='hr-data-broker'",
+        "emx-to-lenel-prod-hr-data-broker");
+    moveAllSpecificMessages(
+        PROD,
+        "watchtower-lenel",
+        "emxSystem='lenel' AND emxErrorRampName='off-ramp'",
+        "emx-to-lenel-prod");
 
-//    moveAllSpecificMessages(PROD, "emx-error", "emxSystem='si-services' AND emxSystemEnvironment='prod'", "emx-to-si-services-prod");
-//    moveAllSpecificMessages(STAGE, "emx-error", "emxSystem='si-services' AND emxSystemEnvironment='test'", "emx-to-si-services-test");
-//    moveAllSpecificMessages(STAGE, "emx-error", "emxSystem='si-services' AND emxSystemEnvironment='stage'", "emx-to-si-services-stage");
-
-//    retrieveMessageFromQpidReplayCache("name='gtmbancoindustrialACH20230411123003061.xml'", "/Users/revloc02/Downloads/gtmbancoindustrialACH20230411123003061.xml");
-
-//    moveAllSpecificMessages(PROD, "emx-error",
-//        "emxSystem='identity-vault' AND emxErrorMessage LIKE '%HttpTimeoutException: request timed out%'",
-//        "emx-to-identity-vault-prod");
-//    moveAllSpecificMessages(PROD, "emx-error",
-//        "emxSystem='identity-vault' AND emxErrorMessage LIKE '%Internal Server Error code: 503%'",
-//        "emx-to-identity-vault-prod");
-//    moveAllSpecificMessages(STAGE, "emx-error",
-//        "emxSystem='identity-vault' AND emxSystemEnvironment='stage'",
-//        "emx-to-identity-vault-stage");
+    //    retrieveMessageFromQpidReplayCache("name='gtmbancoindustrialACH20230411123003061.xml'",
+    // "/Users/revloc02/Downloads/gtmbancoindustrialACH20230411123003061.xml");
   }
 }
