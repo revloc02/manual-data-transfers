a131a762b407393b6adda967c54f083b1a2855c4
revloc02 <4457735+revloc02@users.noreply.github.com>
2026-03-21 05:33:18 -0600

    Update AWS provider version constraint in providers.tf to support newer versions

--- devops/terraform/aws/providers.tf
+++ devops/terraform/aws/providers.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">=2.0,<5.0"
+      version = ">=2.0,<7.0"
     }
   }
   required_version = ">= 1.0,< 2.0"
 }
 provider "aws" {
   region = "us-east-1"
-  # To get cred goto myapplications.microsoft.com > AWS - Identity Center - Enterprise >
+  # To get creds goto myapplications.microsoft.com > AWS - Identity Center - Enterprise >
   # (find the sandbox account) > Access keys > Option 1: Set AWS environment variables >
   # (copy and paste the `export` commands to your cli)
   # shared_credentials_file = "~/.aws/credentials"
